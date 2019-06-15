90cf29c8f65ca80712631ef080ea7d3f0c6f7a3b
Anthony Yang <ay339@cornell.edu>
2019-06-14 22:28:11 -0400

    Attempting to use browser history features

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Router, browserHistory } from 'react-router';
+
 import App from './components/App/index.jsx';
 import * as serviceWorker from './serviceWorker';
 
